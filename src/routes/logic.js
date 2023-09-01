bbd9adb628f39dd9f14824225390ff013caf029d
rajvaya <rajkumarvaya@gmail.com>
2023-09-01 11:25:23 +0530

    update logic.js

--- src/routes/logic.js
+++ src/routes/logic.js
@@ -1,70 +1,48 @@
-console.time("time");
-
-let len = 20000;
-
 function generateRandomGrid(L, W, maxLimit) {
   let grid = [];
-
   for (let l = 0; l < L; l++) {
     let row = [];
     for (let w = 0; w < W; w++) {
-      let randomNumber = Math.floor(Math.random() * (maxLimit + 1)); // Generates random number from 0 to maxLimit
+      let randomNumber = Math.floor(Math.random() * maxLimit) + 1;
       row.push(randomNumber);
     }
     grid.push(row);
   }
-
   return grid;
 }
 
-grid = generateRandomGrid(100000, 100000, 0);
-let cost = 0;
+const directions = [
+  { x: 0, y: -1 }, // top
+  { x: -1, y: 0 }, // left
+  { x: 0, y: 1 }, // bottom
+  { x: 1, y: 0 }, // right
+];
+
 function calculateCost(grid) {
+  console.time("time")
+  let totalCost = 0;
   for (let l = 0; l < grid.length; l++) {
     for (let w = 0; w < grid[l].length; w++) {
       let currFloor = grid[l][w];
-
-      // console.clear()
-      // console.log(`curren index ${l} - ${w} : ${currFloor}`);
-
-      // check left
-      if (grid?.[l - 1]?.[w] == undefined) {
-        cost += currFloor;
-      } else {
-        if (currFloor > grid[l - 1][w]) {
-          cost += currFloor - grid[l - 1][w];
-        }
-      }
-
-      // check bottom
-      if (grid?.[l]?.[w + 1] == undefined) {
-        cost += currFloor;
-      } else {
-        if (currFloor > grid[l][w + 1]) {
-          cost += currFloor - grid[l][w + 1];
-        }
-      }
-      // check right
-      if (grid?.[l + 1]?.[w] == undefined) {
-        cost += currFloor;
-      } else {
-        if (currFloor > grid[l + 1][w]) {
-          cost += currFloor - grid[l + 1][w];
-        }
-      }
-
-      // check top
-      if (grid?.[l]?.[w - 1] == undefined) {
-        cost += currFloor;
-      } else {
-        if (currFloor > grid[l][w - 1]) {
-          cost += currFloor - grid[l][w - 1];
+      for (const direction of directions) {
+        let newL = l + direction.x;
+        let newW = w + direction.y;
+        if (grid?.[newL]?.[newW] === undefined) {
+          totalCost += currFloor;
+        } else if (currFloor > grid[newL][newW]) {
+          totalCost += currFloor - grid[newL][newW];
         }
       }
     }
   }
+  console.timeEnd("time");
+  console.log(totalCost);
 }
 
-calculateCost(grid);
-console.log("total cost :", cost);
-console.timeEnd("time");
+let data = [
+  [1, 3, 4],
+  [2, 2, 3],
+  [1, 2, 4],
+]
+// calculateCost(data);
+calculateCost(generateRandomGrid(10000, 10000, 1000));
